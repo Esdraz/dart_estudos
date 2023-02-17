ae9970342004305f6947e38f0366abf971d08823
Francisco <fsousa.jpa@gmail.com>
2023-02-17 16:28:32 -0300

    add: variaveis

--- bin/dart_estudos.dart
+++
@@ -1,5 +0,0 @@
-import 'package:dart_estudos/dart_estudos.dart' as dart_estudos;
-
-void main(List<String> arguments) {
-  print('Hello world: ${dart_estudos.calculate()}!');
-}
---
+++ lib/01_dart_fundamentos/01_variaveis/01_variaveis.dart
@@ -0,0 +1,7 @@
+void main() {
+  int idade = 31;
+  double precoProduto = 22.50;
+  String nome = 'Esdraz';
+
+  print('nome = $nome, $idade e preço = $precoProduto');
+}
