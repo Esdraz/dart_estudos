e0255c36150f010bb9e126a7a98c80e6c08ed596
Francisco <fsousa.jpa@gmail.com>
2023-04-08 17:34:31 -0300

    update: inicializadores

--- lib/03_dart_poo/03_inicializadores/02_null_safety.dart
+++ lib/03_dart_poo/03_inicializadores/02_null_safety.dart
@@ -1,10 +1,18 @@
+import 'package:dart_estudos/01_dart_fundamentos/04_tratamento_nulos/02_null_aware_operator.dart';
+
 import 'cliente.dart';
 
 // TOME MUITO CUIDADO COM O LATE E O ! (FORCE NON-NULL)
 void main() {
   var cliente = Cliente(nome: 'Francisco');
-  cliente.nome = 'FranciscoS';
+  cliente.nome = 'Francisco';
   print(cliente.nome);
   // cliente.nome = 'Pedro';
   print(cliente.nome);
+
+  print(cliente.idade?.toLowerCase() ?? 'idade não informada!');
+
+  nome = 'Francisco S';
+  print(nome);
+  nome = 'Sousa';
 }
--- lib/03_dart_poo/03_inicializadores/cliente.dart
+++ lib/03_dart_poo/03_inicializadores/cliente.dart
@@ -1,5 +1,6 @@
 class Cliente {
   // late - att inicializado depois (late sobresceve quaisquer regras ex. do final)
   late final String nome;
+  String? idade;
   Cliente({required String nome});
 }
