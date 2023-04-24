87a9c6c7fbcec872046b94873f835536a5b9228e
Francisco <fsousa.jpa@gmail.com>
2023-04-24 20:45:45 -0300

    add: operator methods

---
+++ lib/03_dart_poo/13_operator_methods/numero.dart
@@ -0,0 +1,13 @@
+class Numero {
+  int i;
+  Numero(this.i);
+
+  // metodos utiliza o flag operator e só aceita caracteres de operacao
+  Numero operator +(Numero numero2) {
+    return Numero((i + numero2.i) * 2);
+  }
+
+  Numero operator -(Numero numero2) {
+    return Numero(i - numero2.i);
+  }
+}
