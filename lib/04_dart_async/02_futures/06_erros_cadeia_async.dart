75c3921d3e7377d4ee3f8e71a0a1d05897cd9377
Francisco <fsousa.jpa@gmail.com>
2023-05-10 16:57:49 -0300

    add: erros cadeia async

---
+++ lib/04_dart_async/02_futures/06_erros_cadeia_async.dart
@@ -0,0 +1,19 @@
+void main() {
+  func1().then((value) => func2()).then((value) => func3()).then((print));
+}
+
+Future<String> func1() {
+  print('func1');
+  return Future.value('Fazendo algo func1');
+}
+
+Future<String> func2() {
+  print('func2');
+  // throw Exception();
+  return Future.error('Fazendo algo func2');
+}
+
+Future<String> func3() {
+  print('func3');
+  return Future.value('Fazendo algo func3');
+}
